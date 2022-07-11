af25df4a269565475dd54230ec4cef7d45a56aa4
Arthur Carvalho Gomes Junior <arthur360thug@gmail.com>
2022-07-11 13:19:07 -0400

    Update twiiter.py
    corrigindo erro de sintaxe

--- twiiter.py
+++ twiiter.py
@@ -2,7 +2,7 @@ import tweepy
 import time
 
 #keys e token da api dp twitter para dar acesso a conta pelo codigo
-auth = tweepy.OAutHandler("RXv2Kthb2RHTFDgsp9fMOGwMu","kOdDq4IDWQpOngvb6FcU2FfR7k5Sfmc6OkKcKHivH5bdGHt10B")
+auth = tweepy.OAuthHandler("RXv2Kthb2RHTFDgsp9fMOGwMu","kOdDq4IDWQpOngvb6FcU2FfR7k5Sfmc6OkKcKHivH5bdGHt10B")
 auth.set_acess_token("1546240167744360454-k6Y7sHy7b2qWvwfzZfbuV7h0Dkr2qd","eLfWHDe0o5HHfjGaLBuTyJAqKz0o5i7ZWQ9bHrzaqImfz")
 #limite de vezes que o bot retuita
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
